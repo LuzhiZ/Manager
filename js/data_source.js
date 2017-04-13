ae1759c16e966257f7feaab93c7854cc272223e2
LuzhiZ <luzhizhaowen@foxmail.com>
2017-04-13 11:39:49 +0800

    classify document by postfix

---
+++ js/data_source.js
@@ -0,0 +1,67 @@
+var array=[
+		{
+			"name":"server",	
+			"uuid":0,
+			"parent":null,
+			"type":"node"
+		},
+		{
+			"name":"msg_hub1",	
+			"uuid":0,
+			"parent":null,
+			"type":"node"
+		},
+		{
+			"name":"msg_hub2",	
+			"uuid":0,
+			"parent":null,
+			"type":"node"
+		},
+		{
+			"name":"cell1",	
+			"uuid":0,
+			"parent":null,
+			"type":"node"
+		},
+		{
+			"name":"cell2",	
+			"uuid":0,
+			"parent":null,
+			"type":"node"
+		},
+		{
+			"name":"ws_port",	
+			"uuid":0,
+			"parent":2,
+			"type":"plugin"
+		},
+		{
+			"name":"login_verify",	
+			"uuid":0,
+			"parent":2,
+			"type":"plugin"
+		},
+		{
+			"name":"friend_list",	
+			"uuid":0,
+			"parent":2,
+			"type":"plugin"
+		}
+		
+];
+var jsonEdges=[
+		{"from":0,"to":1},
+
+
+		{"from":1,"to":3},
+
+
+		{"from":4,"to":2},
+
+
+		{"from":2,"to":0},
+
+		{"from":5,"to":6},
+		{"from":6,"to":7}
+];
+var jsontest={ "name": "cxh", "sex": "man" };
